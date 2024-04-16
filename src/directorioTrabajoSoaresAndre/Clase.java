688f21fa685649978324073ad85b1a212f4a63f9
unknown <DAM125@iesataulfo.local>
2024-04-16 16:22:30 +0200

    cuartaFase

--- src/directorioTrabajoSoaresAndre/Clase.java
+++ src/directorioTrabajoSoaresAndre/Clase.java
@@ -2,4 +2,10 @@ package directorioTrabajoSoaresAndre;
 
 public class Clase {
 //Tercera fase
+	
+	
+	
+	public Clase() {
+		System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	}
 }
